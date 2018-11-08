c22e4eee24d2e2e7e6deb6ab6bb0a83b51bd79b0
Kyle Bateman <kyle@lux0.batemans.org>
2018-11-08 10:29:51 -0700

    Add bus module with separate event flags

--- src/common.js
+++ src/common.js
@@ -3,6 +3,8 @@
 // -----------------------------------------------------------------------------
 //Components are responsible to tap into this object and bind to any
 //appropriate properties.
+//- TODO:
+//- Why is 'vm' an argument to messageBus
 //- 
 import Wyseman from './wyseman.js'
 var docIndex = 0
@@ -19,7 +21,7 @@ module.exports = {
     })
   },
   
-  messageBus(vm) {
+  messageBus(vm) {		//Bus for messages to be distributed to registered clients
     this.master = vm
     this.clients = {}
     
@@ -31,7 +33,7 @@ module.exports = {
         delete this.clients[id]
     },
     
-    this.notify = function(message, data) {
+    this.notify = function(message, data) {	//Every registered client will get every message
       let replies = []
 //console.log("Bus notify:", this.clients)
       Object.keys(this.clients).forEach(key => {
@@ -39,6 +41,27 @@ module.exports = {
         replies.push(this.clients[key](message, data))
       })
 //console.log("Bus ans:", this.master, replies)
+      return replies
+    }
+  },
+
+  eventBus() {			//Like messageBus, but clients can listen for specific events
+    this.events = {}
+    this.register = function(id, event, cb) {		//I:ID am listening for events:event
+      if (!(event in this.events)) this.events[event] = {}
+//console.log("Register id:", id, "event:", event)
+      if (cb) {						//Registering
+        if (!(id in this.events[event])) this.events[event][id] = cb
+      } else if (id in this.events[event]) {		//De-registering
+        delete this.events[events][id]
+      }
+    }
+    this.notify = function(event, data) {			//Invoke all listener callbacks for: event
+      let replies = []
+//console.log("Notify event:", event, this.events[event])
+      if (this.events[event]) Object.keys(this.events[event]).forEach(id => {
+        replies.push(this.events[event][id](data))
+      })
       return replies
     }
   },
