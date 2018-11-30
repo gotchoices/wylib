823ed61c2ec78b4b39a5473df3c992bded9ead07
Kyle Bateman <kyle@lux2.batemans.org>
2018-11-30 00:14:14 -0700

    Submenus post in a reasonable place

--- src/menu.vue
+++ src/menu.vue
@@ -2,6 +2,7 @@
 //Copyright WyattERP.org: GNU GPL Ver 3; see: License in root of this package
 // -----------------------------------------------------------------------------
 //TODO:
+//- Register single event listener for toplevel window to unpost all menus (rather than having every menu register an event listener)
 //- Configuration separate from contents?
 //- Contents gives actual menu items
 //- Can have multiple text fields, icons, cascades
@@ -17,7 +18,7 @@
   <div class="wylib wylib-menu">
     <div class="menu" title=''>
       <table>
-        <tr v-for="item in config" :key="item.idx" @click="execute(item.call)" v-on:mouseenter="enterItem(item.idx, item.menu)" :title="(item.lang?item.lang.help:null)">
+        <tr v-for="item in config" :key="item.idx" @click="execute(item.call)" v-on:mouseenter="enterItem($event, item.idx, item.menu)" :title="(item.lang?item.lang.help:null)">
           <td v-for="fld in layout" :key="fld">
             <svg v-if="fld=='icon'" class="icon" style="height:1em; width:1em;" v-html="iconSvg(item.icon)"></svg>
             <div v-else-if="fld=='lang'">{{ (item.lang?item.lang.title:null) || item.idx }}</div>
@@ -29,7 +30,7 @@
       </table>
     </div>
     <div class="submenus">
-      <wylib-win v-for="item in config" v-if="item.menu" :state="state.subs[item.idx]" :key="item.idx" pinnable=true @close="state.subs[item.idx].posted=false" :lang="item.lang">
+      <wylib-win v-for="item in config" v-if="item.menu" ref="submenu" :state="state.subs[item.idx]" :key="item.idx" pinnable=true @close="state.subs[item.idx].posted=false" :lang="item.lang">
         <wylib-menu :state="state.subs[item.idx].client" :config="item.menu" @done="state.subs[item.idx].posted = state.subs[item.idx].pinned; $emit('done')"/>
       </wylib-win>
     </div>
@@ -56,13 +57,30 @@ export default {
     iconSvg(icon) {
       return Icons(icon)
     },
-    enterItem(idx, itemMenu) {
-console.log("Entering menu item: ", idx, "itemMenu:", itemMenu, this.state.subs)
+    enterItem(ev, idx, itemMenu) {
+//console.log("Entering menu item: ", ev, idx, "itemMenu:", itemMenu, this.state.subs)
       if (this.state.subs) {
         Object.keys(this.state.subs).forEach(key => {	//Close any subwindows when moving around
-          if (this.state.subs[key]) this.state.subs[key].posted = false
+          if (this.state.subs[key] && !this.state.subs[key].pinned) this.state.subs[key].posted = false
         })
-        if (itemMenu && this.state.subs[idx]) this.state.subs[idx].posted = true	//But open any submenu associated with this line
+        let theSub = this.state.subs[idx]
+        if (itemMenu && theSub) {			//But open any submenu associated with this line
+          theSub.posted = true
+          if (theSub.x == null || theSub.y == null) {	//If this is the first time posted
+//console.log("Refs:", this.$refs)
+            let itemBBox = ev.target.getBoundingClientRect()
+              , menuBBox = ev.target.closest('.wylib-menu').getBoundingClientRect()
+              , viewWidth = (window.innerWidth || document.documentElement.clientWidth)
+              , rightGap = viewWidth - (menuBBox.x + menuBBox.width)
+              , subComp = this.$refs.submenu.find(cur=>{return (cur.state == theSub)})
+              , subElem = subComp ? subComp.$el : null
+              , subBBox = subElem ? subElem.getBoundingClientRect() : null
+              , subWidth = subBBox ? subBBox.width : 200
+//console.log("Posting sub:", theSub.x, theSub.y, "Item:",itemBBox, "Menu:",menuBBox, viewWidth, "Comp:", subComp, "Elem:", subElem)
+            theSub.y = menuBBox.height - (2 * itemBBox.height)		//Place the menu relevant to the invoking menu item
+            theSub.x = (rightGap < subWidth) ? -(menuBBox.width + subWidth) : 0
+          }
+        }
       }
 //console.log("  Posted: ", this.state.subs[idx])
     },
@@ -78,7 +96,7 @@ console.log("Entering menu item: ", idx, "itemMenu:", itemMenu, this.state.subs)
     Com.react(this, {subs: {}})
     this.config.forEach((item, x) => {		//Set object properties that are not known until now
       if (item.menu) {
-console.log("Set default for: ", item.idx)
+//console.log("Set default for: ", item.idx)
         this.$set(this.state.subs, item.idx, {posted: false, pinned: false, client: {}})
       }
     })
@@ -104,6 +122,7 @@ console.log("Set default for: ", item.idx)
     position: relative;
     top: 3px;			//Fixme: can do with padding or borders?
     left: 3px;			//Fixme: can do with padding or borders?
+    margin: 1px 5px 4px 0px;
 //    overflow-x: hidden;
 //    overflow-y: scroll;
 //    border: 1px solid purple;
