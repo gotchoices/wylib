6ea5d0748176ffa6309909c892b96ea2e74a1e94
Kyle Bateman <kyle@lux2.batemans.org>
2018-12-04 12:16:24 -0700

    Work with state save/restore menu

--- src/menu.vue
+++ src/menu.vue
@@ -18,20 +18,20 @@
   <div class="wylib wylib-menu">
     <div class="menu" title=''>
       <table>
-        <tr v-for="item in config" :key="item.idx" @click="execute(item.call)" v-on:mouseenter="enterItem($event, item.idx, item.menu)" :title="(item.lang?item.lang.help:null)">
+        <tr v-for="item in config" :key="item.idx" @click="execute(item.call)" v-on:mouseenter="enterItem($event, item)" :title="(item.lang?item.lang.help:null)">
           <td v-for="fld in layout" :key="fld">
             <svg v-if="fld=='icon'" class="icon" style="height:1em; width:1em;" v-html="iconSvg(item.icon)"></svg>
             <div v-else-if="fld=='lang'">{{ (item.lang?item.lang.title:null) || item.idx }}</div>
             <input v-else-if="fld=='input' && item.input" :type="item.input"/>
             <svg v-else-if="fld=='input' && item.menu" class="icon" style="height: 1em; width: 1em" v-html="iconSvg('play3')"></svg>
-            <div v-else-if="fld==item.idx">{{ item[fld] }}</div>
+            <div v-else>{{ item[fld] }}</div>
           </td>
         </tr>
       </table>
     </div>
     <div class="submenus">
       <wylib-win v-for="item in config" v-if="item.menu" ref="submenu" :state="state.subs[item.idx]" :key="item.idx" pinnable=true @close="state.subs[item.idx].posted=false" :lang="item.lang">
-        <wylib-menu :state="state.subs[item.idx].client" :config="item.menu" @done="state.subs[item.idx].posted = state.subs[item.idx].pinned; $emit('done')"/>
+        <wylib-menu :state="state.subs[item.idx].client" :config="item.menu" :layout="item.layout?item.layout:layout" @done="state.subs[item.idx].posted = state.subs[item.idx].pinned; $emit('done')"/>
       </wylib-win>
     </div>
   </div>
@@ -53,55 +53,61 @@ export default {
   data() { return {
     pr:		require('./prefs'),
   }},
+  computed: {
+  },
   methods: {
     iconSvg(icon) {
       return Icons(icon)
     },
-    enterItem(ev, idx, itemMenu) {
-//console.log("Entering menu item: ", ev, idx, "itemMenu:", itemMenu, this.state.subs)
-      if (this.state.subs) {
-        Object.keys(this.state.subs).forEach(key => {	//Close any subwindows when moving around
-          if (this.state.subs[key] && !this.state.subs[key].pinned) this.state.subs[key].posted = false
-        })
-        let theSub = this.state.subs[idx]
-        if (itemMenu && theSub) {			//But open any submenu associated with this line
-          theSub.posted = true
-          if (theSub.x == null || theSub.y == null) {	//If this is the first time posted
+    enterItem(ev, item) {
+      let idx = item.idx, theSubs = this.state.subs, theSub = theSubs[idx]
+//console.log("Entering item: ", idx, "menu:", item.menu, "Subs:", theSubs, "Sub:", theSub)
+      if (!theSubs || theSubs.length <= 0) return
+      Object.keys(this.state.subs).forEach(key => {	//Close any subwindows when moving around
+        if (theSubs[key] && !theSubs[key].pinned) theSubs[key].posted = false
+      })
+
+      if (item.menu && theSub) {			//But open any submenu associated with this line
+        theSub.posted = true
+        if (theSub.x == null || theSub.y == null) {	//If this is the first time posted
 //console.log("Refs:", this.$refs)
-            let itemBBox = ev.target.getBoundingClientRect()
-              , menuBBox = ev.target.closest('.wylib-menu').getBoundingClientRect()
-              , viewWidth = (window.innerWidth || document.documentElement.clientWidth)
-              , rightGap = viewWidth - (menuBBox.x + menuBBox.width)
-              , subComp = this.$refs.submenu.find(cur=>{return (cur.state == theSub)})
-              , subElem = subComp ? subComp.$el : null
-              , subBBox = subElem ? subElem.getBoundingClientRect() : null
-              , subWidth = subBBox ? subBBox.width : 200
+          let itemBBox = ev.target.getBoundingClientRect()
+            , menuBBox = ev.target.closest('.wylib-menu').getBoundingClientRect()
+            , viewWidth = (window.innerWidth || document.documentElement.clientWidth)
+            , rightGap = viewWidth - (menuBBox.x + menuBBox.width)
+            , subComp = this.$refs.submenu.find(cur=>{return (cur.state == theSub)})
+            , subElem = subComp ? subComp.$el : null
+            , subBBox = subElem ? subElem.getBoundingClientRect() : null
+            , subWidth = subBBox ? subBBox.width : 200
 //console.log("Posting sub:", theSub.x, theSub.y, "Item:",itemBBox, "Menu:",menuBBox, viewWidth, "Comp:", subComp, "Elem:", subElem)
-            theSub.y = menuBBox.height - (2 * itemBBox.height)		//Place the menu relevant to the invoking menu item
-            theSub.x = (rightGap < subWidth) ? -(menuBBox.width + subWidth) : 0
-          }
+          theSub.y = itemBBox.y - menuBBox.y - itemBBox.height		//Place the menu relevant to the invoking menu item
+          theSub.x = (rightGap < subWidth) ? -(menuBBox.width + subWidth) : 0
         }
       }
-//console.log("  Posted: ", this.state.subs[idx])
+//console.log("  Posted: ", theSub)
     },
     execute(cb) {		//Execute the specified callback
       this.$emit('done')
       if (cb) cb()
     },
   },
+
   beforeCreate: function() {
     this.$options.components['wylib-win'] = require('./win.vue').default
   },
+
   created: function() {
     Com.react(this, {subs: {}})
-    this.config.forEach((item, x) => {		//Set object properties that are not known until now
-      if (item.menu) {
-//console.log("Set default for: ", item.idx)
-        this.$set(this.state.subs, item.idx, {posted: false, pinned: false, client: {}})
+//console.log("Menu state: ", JSON.stringify(this.state))
+
+    this.config.forEach((item, x) => {		//Set any object properties that are not known until now
+      if (item.menu && !this.state.subs[item.idx]) {
+        this.$set(this.state.subs, item.idx, {posted: false, pinned: false, x:null, y:null, client: {}})
+//console.log("Set default for: ", item.idx, "State:", this.state.subs[item.idx])
       }
     })
-//console.log("State client: ", JSON.stringify(this.state))
   },
+
   mounted: function() {
 //console.log("Menu components: " + JSON.stringify(this.$options.components))
   },
@@ -120,9 +126,9 @@ export default {
   .wylib-menu > .menu {
     width: calc(100% - 6px);	//Fixme: can do with padding or borders?
     position: relative;
-    top: 3px;			//Fixme: can do with padding or borders?
-    left: 3px;			//Fixme: can do with padding or borders?
-    margin: 1px 5px 4px 0px;
+//    top: 3px;			//Fixme: can do with padding or borders?
+//    left: 3px;			//Fixme: can do with padding or borders?
+//    margin: 1px 1px 1px 1px;
 //    overflow-x: hidden;
 //    overflow-y: scroll;
 //    border: 1px solid purple;
@@ -137,6 +143,9 @@ export default {
   }
   .wylib-menu .menu td {
     white-space: nowrap;
+    padding-left: 4px;
+    padding-right: 4px;
+    border: 1px solid #f2f2f2;
   }
   .wylib-menu .icon {
     display: block;
